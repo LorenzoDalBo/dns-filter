4dfc9083fe1617d2aaa383cc364a030f2b105f5f
Lorenzo <lorenzotrevizoldalbo@gmail.com>
2026-03-25 19:50:47 -0300

    feat(config): add YAML configuration file support (RNF04.1)

---
+++ internal/config/defaults.go
@@ -0,0 +1,33 @@
+package config
+
+// Defaults returns a Config with sensible production defaults.
+func Defaults() *Config {
+	return &Config{
+		DNS: DNSConfig{
+			Listen:    "0.0.0.0:53",
+			Upstreams: []string{"8.8.8.8:53", "8.8.4.4:53", "1.1.1.1:53"},
+			BlockIP:   "0.0.0.0",
+			PortalIP:  "0.0.0.0",
+		},
+		Cache: CacheConfig{
+			TTLFloorSeconds:   30,
+			TTLCeilingSeconds: 3600,
+		},
+		API: APIConfig{
+			Listen:    ":8081",
+			JWTSecret: "",
+		},
+		Captive: CaptiveConfig{
+			Listen:     ":80",
+			SessionTTL: 8,
+		},
+		DB: DBConfig{
+			URL:           "postgres://dnsfilter:dnsfilter123@localhost:5432/dnsfilter?sslmode=disable",
+			RetentionDays: 120,
+			LogBufferSize: 100000,
+		},
+		Log: LogConfig{
+			Level: "info",
+		},
+	}
+}
